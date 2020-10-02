8349407ca84867f1f3e17f083943393787f84917
anikett-rpx <anikett_c@rpxcorp.com>
2020-10-02 13:03:04 +0530

    Added bootstrap template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <h1>Welcome</h1>
+    <div className="container">
+      <div className="row">
+        <div className="col-sm">
+          <div className="card" >
+            <img src="https://picsum.photos/200/301" className="card-img-top" alt="..." width="100px"  height="200px" />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" className="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+        <div classNameName="col-sm">
+          <div className="card" >
+          <img src="https://picsum.photos/200/301" className="card-img-top" alt="..." width="100px" height="200px"/>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" className="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+        </div>
+        <div classNameName="col-sm">
+          <div className="card" >
+          <img src="https://picsum.photos/200/301" className="card-img-top" alt="..." width="100px" height="200px"/>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" className="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+        </div>
+      </div>
     </div>
+  </>
   );
 }
 
